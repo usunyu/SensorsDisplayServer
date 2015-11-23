0d336a553108c9a601e3336eb6beea0ac5a414ff
usunyu <usunyu@gmail.com>
2015-11-23 02:56:09 -0800

    draw chart using smoothie

--- django-env/sensors_display/apis.py
+++ django-env/sensors_display/apis.py
@@ -2,21 +2,27 @@ from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-TITLE_LIST = []
-DATA_BUFFER = []
-
 class WriteSensorsData(APIView):
     permission_classes = (permissions.AllowAny,)
 
-    def get(self, request, format=None):
+    def post(self, request, format=None):
 
-        print 'has been called'
+        ReadSensorsData.CURRENT_SENSOR_TYPE = request.data['sensorType']
+        ReadSensorsData.CURRENT_SENSOR_VALUES = request.data['sensorValues']
 
         return Response(status=status.HTTP_200_OK)
 
 class ReadSensorsData(APIView):
     permission_classes = (permissions.AllowAny,)
 
+    CURRENT_SENSOR_TYPE = ''
+    CURRENT_SENSOR_VALUES = []
+
     def get(self, request, format=None):
 
-        return Response(status=status.HTTP_200_OK)
+        result = {
+            'sensorType': self.CURRENT_SENSOR_TYPE,
+            'sensorValues': self.CURRENT_SENSOR_VALUES,
+        }
+
+        return Response(result, status=status.HTTP_200_OK)
--- django-env/sensors_display/wsgi.py
+++ django-env/sensors_display/wsgi.py
@@ -13,4 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensors_display.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+# try:
+from dj_static import Cling
+application = Cling(get_wsgi_application())
+# except:
+    # pass
